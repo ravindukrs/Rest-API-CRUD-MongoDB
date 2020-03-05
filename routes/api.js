090e67a1bd233594a264ba1ae7acbfa4e37d3868
Ravindu Fernando <ravindupf@gmail.com>
2020-03-05 22:28:00 +0530

    Created complete front end using react components

--- routes/api.js
+++ routes/api.js
@@ -4,21 +4,39 @@ const User = require('../models/user');
 
 //Get list of users
 router.get('/users', function (req, res, next) {
-    User.aggregate([
-        {
-          $geoNear: {
-             near: { type: "Point", coordinates: [ parseFloat(req.query.lng) , parseFloat(req.query.lat) ] },
-             distanceField: "dist.calculated",
-             maxDistance: 100000,
-             includeLocs: "dist.location",
-             spherical: true
-          }
-        }
-     ]).then(function(users){
-         res.send(users);
-     }).catch(next);
-   
+    if(req.query.id != undefined){
+        console.log("ID Defined, Search by ID");
+        User.find({id:req.query.id}).then(function (users) {
+            res.send(users);
+        }).catch(next);
+    }else if (req.query.name != undefined){
+        console.log("Name Defined, Search by Name");
+        User.find({name:req.query.name}).then(function (users) {
+            res.send(users);
+        }).catch(next);
+    }else if (req.query.lat == undefined || req.query.lng == undefined || req.query.lat == "" || req.query.lng == "") {
+        console.log("Lat and Long Not Defined, Sending all Users");
+        User.find({}).then(function (users) {
+            res.send(users);
+        }).catch(next);
+    } else if ((req.query.lat != undefined) && (req.query.lng != undefined) && (req.query.lat != "") && (req.query.lng != "")) {
+        console.log("Lat and Long Defined");
+        User.aggregate([
+            {
+                $geoNear: {
+                    near: { type: "Point", coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)] },
+                    distanceField: "dist.calculated",
+                    maxDistance: 100000,
+                    includeLocs: "dist.location",
+                    spherical: true
+                }
+            }
+        ]).then(function (users) {
+            res.send(users);
+        }).catch(next);
+    }
 })
+
 
 //Add new user
 router.post('/users', function (req, res, next) {
